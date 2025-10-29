b692d25a829753b75f2d3b2dd1a3f498235a954d
kumarsantos <ssah5803@gmail.com>
2025-10-29 22:48:13 +0530

    support newline as delimeter

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,6 +1,6 @@
 export const stringCalculator = (input: string): number => {
   if (!input) return 0;
 
-  const numbers = input.split(",").map(Number);
+  const numbers = input.split(/,|\n/).map(Number);
   return numbers.reduce((a, b) => a + b, 0);
 };
